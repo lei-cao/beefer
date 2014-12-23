8a5bbd5fab2535d6c7882359d511bb910014539f
Lei Cao <lei.cao@zalora.com>
2014-12-23 11:47:06 +0800

    02 hello beego

--- beefer.go
+++ beefer.go
@@ -2,6 +2,16 @@ package main
 
 import "github.com/astaxie/beego"
 
+type BeeferController struct {
+	beego.Controller
+}
+
+// The Get method to handle the GET request
+func (c *BeeferController) Get() {
+	c.Ctx.WriteString("Hello Beego")
+}
+
 func main() {
-    beego.Run(":8080")
+	beego.Router("/", &BeeferController{})
+	beego.Run(":8080")
 }
