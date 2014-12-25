18085d29ce7b188ed50443816beab517e25f201c
Lei Cao <lei.cao@zalora.com>
2014-12-26 01:25:46 +0800

    03: template embeded

--- beefer.go
+++ beefer.go
@@ -1,14 +1,39 @@
 package main
 
-import "github.com/astaxie/beego"
+import (
+	"text/template"
+
+	"github.com/astaxie/beego"
+)
 
 type BeeferController struct {
 	beego.Controller
 }
 
+type User struct {
+	Username string
+}
+
 // The Get method to handle the GET request
 func (c *BeeferController) Get() {
-	c.Ctx.WriteString("Hello Beego")
+	var tpl string = `
+        <html>
+            <head>
+                <title>Beefer!</title>
+            </head>
+            <body>
+                <strong>Hello, {{.User.Username}}</strong>
+            </body>
+        </html>
+    `
+	data := make(map[interface{}]interface{})
+	user := User{Username: "Alice"}
+	data["User"] = user
+
+	t := template.New("Beefer Template")
+	t = template.Must(t.Parse(tpl))
+
+	t.Execute(c.Ctx.ResponseWriter, data)
 }
 
 func main() {
